fdcc5aec57494ac4fce0c605e6cedc215c0a9109
anoshc <anoshc@stud.ntnu.no>
2022-02-22 23:38:49 +0100

    Started to implement the javascript code

---
+++ script.js
@@ -0,0 +1,13 @@
+// Hent html elementene med id og class navn
+const strek = document.getElementById("strek");
+const spillSlutt = document.getElementsByClassName("spill-slutt");
+const spillVinner = document.getElementsByClassName("spill-vinner");
+const prøvIgjen = document.getElementsByClassName("prøv-igjen");
+
+// Definer en variabel som velger alle rutene i html med class-navnet "rute" 
+const ruter = document.querySelectorAll(".rute");
+
+// Definer variabler for spiller X og O, og en tredje variabel som sjekker hvem sin tur det er
+const playerX = "X";
+const playerO = "O";
+let tur = playerX;
