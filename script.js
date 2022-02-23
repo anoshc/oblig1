98de76cfbd2eafa6dcc7acf7f4ec4be7b5fa5965
anoshc <anoshc@stud.ntnu.no>
2022-02-23 18:13:31 +0100

    Added js code with the winning conditions

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
-// Henter html elementene med id og class navn
-const brikke = document.getElementById("brikke");
-const spillSlutt = document.getElementById("spill-slutt");
-const spillVinner = document.getElementsByClassName("spill-vinner");
-const prøvIgjen = document.getElementsByClassName("prøv-igjen");
-
-// Definerer en variabel som velger alle rutene i html med class-navnet "rute" 
-const ruter = document.querySelectorAll(".rute");
-
-// Definerer variabler for spiller X og O, og en tredje variabel som sjekker hvem sin tur det er
+const ruter = document.querySelectorAll(".rute")
 const playerX = "X";
 const playerO = "O";
 let tur = playerX;
 
-//
 const brettState = Array(ruter.length);
 brettState.fill(null);
 
-// Legger til eventListner til hver rute på brettet
+const brikke = document.getElementById("brikke");
+const spillSlutt = document.getElementById("spill-slutt");
+const tekstVinner = document.getElementsByClassName("tekst-vinner");
+const spillIgjen = document.getElementsByClassName("prøv-igjen");
+
 ruter.forEach((rute) => rute.addEventListener("click", ruteClick));
 
-
-// Innfører funksjonen "ruteClick", som vil se hvilken rute som er klikket på
 function ruteClick(event) {
     if (spillSlutt.classList.contains("synlig")) {
         return;
@@ -28,36 +20,31 @@ function ruteClick(event) {
 
     const rute = event.target;
     const ruteNummer = rute.dataset.index;
-
-    //if its not equal to blank it means an x or o must be inside there
     if (rute.innerText != "") {
         return;
     }
 
-    //check who´s turn it is
     if (tur === playerX) {
         rute.innerText = playerX;
-        brettState[ruteNummer - 1] = playerX;
+        brettState[ruteNummer] = playerX;
         tur = playerO;
     } else {
         rute.innerText = playerO;
-        brettState[ruteNummer - 1] = playerO;
+        brettState[ruteNummer] = playerO;
         tur = playerX;
     }
 
     vinnerStatus();
 }
 
-// Sjekker om det blir vinn eller uavgjort
+// Funksjon som sjekker om det er en vinner
 function vinnerStatus() {
-    for (const vinnerForhold of vinneKombinasjoner) {
-        const { kombinasjon, brikkeClass } = vinnerForhold;
-        //const kombinasjon = vinnerForhold.kombinasjon;
-        //const brikke = vinnerForhold.brikke;
+    for (const vinnerForhold of vinnerKombinasjon) {
 
-        const ruteVerdi1 = brettState[kombinasjon[0] - 1];
-        const ruteVerdi2 = brettState[kombinasjon[1] - 1];
-        const ruteVerdi3 = brettState[kombinasjon[2] - 1];
+        const { kombinasjon, brikkeClass } = vinnerForhold;
+        let ruteVerdi1 = brettState[kombinasjon[0]];
+        let ruteVerdi2 = brettState[kombinasjon[1]];
+        let ruteVerdi3 = brettState[kombinasjon[2]];
 
         if (
             ruteVerdi1 != null &&
@@ -65,21 +52,22 @@ function vinnerStatus() {
             ruteVerdi1 === ruteVerdi3
         ) {
             brikke.classList.add(brikkeClass);
+
         }
     }
 
-
 }
 
-const vinneKombinasjoner = [
-    { kombinasjon: [1, 2, 3], brikkeClass: "horisontal-linje-1" },
-    { kombinasjon: [4, 5, 6], brikkeClass: "horisontal-linje-2" },
-    { kombinasjon: [7, 8, 9], brikkeClass: "horisontal-linje-3" },
+const vinnerKombinasjon = [
+    { kombinasjon: [0, 1, 2], brikkeClass: "horisontal-linje-1" },
+    { kombinasjon: [3, 4, 5], brikkeClass: "horisontal-linje-2" },
+    { kombinasjon: [6, 7, 8], brikkeClass: "horisontal-linje-3" },
 
-    { kombinasjon: [1, 4, 7], brikkeClass: "vertikal-linje-1" },
-    { kombinasjon: [2, 5, 8], brikkeClass: "vertikal-linje-2" },
-    { kombinasjon: [3, 6, 9], brikkeClass: "vertikal-linje-3" },
+    { kombinasjon: [0, 3, 6], brikkeClass: "vertikal-linje-1" },
+    { kombinasjon: [1, 4, 7], brikkeClass: "vertikal-linje-2" },
+    { kombinasjon: [2, 5, 8], brikkeClass: "vertikal-linje-3" },
 
-    { kombinasjon: [1, 5, 9], brikkeClass: "diagonal-linje-1" },
-    { kombinasjon: [3, 5, 9], brikkeClass: "diagonal-linje-2" }
+    { kombinasjon: [0, 4, 8], brikkeClass: "diagonal-linje-1" },
+    { kombinasjon: [2, 4, 6], brikkeClass: "diagonal-linje-2" },
+
 ]
